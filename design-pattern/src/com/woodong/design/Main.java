9f285871c0e5458dfad7df6f8c27949d5ab67884
wooyoung85 <cerealTigerPower@gmail.com>
2022-12-15 13:16:21 +0900

    observer pattern update

--- design-pattern/src/com/woodong/design/Main.java
+++ design-pattern/src/com/woodong/design/Main.java
@@ -1,19 +1,18 @@
 package com.woodong.design;
 
-import com.woodong.design.decorator.*;
+import com.woodong.design.observer.Button;
+import com.woodong.design.observer.IButtonListener;
 
 public class Main {
     public static void main(String[] args) {
-        ICar audi = new Audi(1000);
-        audi.showPrice();
-
-        ICar a3 = new A3(audi, "A3");
-        a3.showPrice();
-
-        ICar a4 = new A4(audi, "A4");
-        a3.showPrice();
-
-        ICar a5 = new A5(audi, "A5");
-        a5.showPrice();
+        Button button = new Button("버튼");
+        button.addListener(event -> {
+            System.out.println(event);
+        });
+        button.click("메시지 전달 : click 1");
+        button.click("메시지 전달 : click 2");
+        button.click("메시지 전달 : click 3");
+        button.click("메시지 전달 : click 4");
+        button.click("메시지 전달 : click 5");
     }
 }
---
+++ design-pattern/src/com/woodong/design/observer/IButtonListener.java
@@ -0,0 +1,5 @@
+package com.woodong.design.observer;
+
+public interface IButtonListener {
+    void clickEvent(String event);
+}
